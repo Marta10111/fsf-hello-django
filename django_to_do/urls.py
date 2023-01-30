da36c067cd2d9a87b5a6de6f26cd6741bfbc950d
Marta <manta.101@hotmail.com>
2023-01-30 14:02:01 +0000

    Creating a New Item 2

--- django_to_do/urls.py
+++ django_to_do/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from todo.views import get_todo_list, add_item
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', get_todo_list, name='get_todo_list'),
-    path('add', add_item, name='add')
+    path('add', add_item, name='add'),
 ]
