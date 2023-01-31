4f93e0627c8e0df2841277c34e6df6298e281447
Marta <manta.101@hotmail.com>
2023-01-31 10:14:35 +0000

    h

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = '2erm2g25fi&()6+d)rp0(1gw!vp^v2aoxpmfebc^1_2gbjc-@#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['manta101-django-todo-app']
+ALLOWED_HOSTS = ['manta101-django-todo-app.herokuapp.com']
 
 
 # Application definition
